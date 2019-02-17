225812d66455bd4f32ed8f365f423bc3d2e8ea44
jnizet <jb@ninja-squad.com>
2019-02-17 23:58:03 +0100

    test: add integration test for the MockMvc-specific DSL

--- mockmvc/build.gradle.kts
+++ mockmvc/build.gradle.kts
@@ -1,5 +1,10 @@
 dependencies {
     api(project(":core"))
     api("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+    }
 }
 
