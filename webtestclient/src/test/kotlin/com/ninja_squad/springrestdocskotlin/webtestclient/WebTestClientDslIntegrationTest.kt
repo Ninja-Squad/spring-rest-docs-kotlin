5705d93605925085bed843468f9adada98131a51
jnizet <jb@ninja-squad.com>
2019-02-18 12:35:22 +0100

    test: improve WebTestClient integration test to use autoconfiguration

--- webtestclient/src/test/kotlin/com/ninja_squad/springrestdocskotlin/webtestclient/WebTestClientDslIntegrationTest.kt
+++ webtestclient/src/test/kotlin/com/ninja_squad/springrestdocskotlin/webtestclient/WebTestClientDslIntegrationTest.kt
@@ -1,27 +1,23 @@
 package com.ninja_squad.springrestdocskotlin.webtestclient
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
-import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.restdocs.RestDocumentationExtension
-import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.config.EnableWebFlux
 import java.io.File
 
 data class User(val id: Long, val firstName: String, val lastName: String)
 
 @SpringBootApplication
-@EnableWebFlux
 @RestController
 @RequestMapping("/users")
 open class UserController {
@@ -35,22 +31,10 @@ open class UserController {
  * Integration tests for the various docXxx extension methods and for andDocument
  * @author JB Nizet
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebFluxTest
 @ExtendWith(RestDocumentationExtension::class)
-class WebTestClientDslIntegrationTest {
-
-    private lateinit var webClient: WebTestClient
-
-    @BeforeEach
-    fun setUp(
-        applicationContext: ApplicationContext,
-        restDocumentation: RestDocumentationContextProvider
-    ) {
-        this.webClient = WebTestClient.bindToApplicationContext(applicationContext)
-            .configureClient()
-            .filter(WebTestClientRestDocumentation.documentationConfiguration(restDocumentation))
-            .build()
-    }
+@AutoConfigureRestDocs
+class WebTestClientDslIntegrationTest(@Autowired private val webClient: WebTestClient) {
 
     @Test
     fun `should get user`() {
